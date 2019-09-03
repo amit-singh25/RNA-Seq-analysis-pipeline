c3f5de49dbe7f163ebc731ae940fb5a5f1d069cb
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2019-09-03 15:19:30 +0200

    Create  merge_strigte.sh

---
+++ Alignment/merge_strigte.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#PBS -N s_merge
+##PBS -j oe 
+##PBS -l file=100GB
+#PBS-l mem=25G
+#PBS -l walltime=50:13:59
+#PBS -l nodes=1:ppn=8   
+#PBS -o ./s_merge.out
+#PBS -e ./s_merge.err
+#PBS -V 
+
+#echo $PBS_JOBNAME
+#echo $PBS_JOBID
+
+
+
+
+
+name=$1
+put=~/stringte
+dir=~/alignment
+gtf=~/genome/igenome/Mus_musculus/Ensembl/GRCm38/Annotation/Archives/archive-current/Genes
+genome=~/genome/igenome/Mus_musculus/Ensembl/GRCm38/Sequence/WholeGenomeFasta
+
+
+
+
+stringtie --merge -p 16 -G ${gtf}/genes.gtf -o ${put}/stringtie_merged.gtf ${put}/mergelist.txt
