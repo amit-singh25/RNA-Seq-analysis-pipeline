ebe57a75e1c923af00f1e2ba5dba6cae9c01cc79
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2019-09-03 14:34:44 +0200

    Update Trim_qc.sh

--- Quality_Check/Trim_qc.sh
+++ Quality_Check/Trim_qc.sh
@@ -1,1 +1,15 @@
-Quality check
+#!/bin/bash
+#PBS -N fastqc
+#PBS -j oe 
+##PBS -l file=16GB
+#PBS-l mem=100G
+#PBS -l walltime=50:13:59
+#PBS -l nodes=1:ppn=8   
+
+name=$1
+
+data=~/mouse_data
+out=~/fastqc
+fastqc ${data}${name}.fastq.gz > ${out}${name}
+echo ${name}
+
