47c713fd26d03fcf956016798e6e8fd3019c2451
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2019-09-03 15:23:56 +0200

    Update cufflink.sh

--- Alignment/cufflink.sh
+++ Alignment/cufflink.sh
@@ -1,1 +1,21 @@
 
+#!/bin/bash
+#PBS -N cufflink 
+#PBS -j oe 
+##PBS -l file=100GB
+#PBS-l mem=100G
+#PBS -l walltime=50:13:59
+#PBS -l nodes=1:ppn=8   
+
+name=$1
+out=~/cuff_out_out
+dir=~/alignemnt
+gtf=~/genome/igenome/Homo_sapiens/Ensembl/GRCh37/Annotation/Archives/archive-current/Genes
+genome=~/genome/igenome/Homo_sapiens/Ensembl/GRCh37/Sequence/WholeGenomeFasta
+
+cufflinks -p 8 -o $(out)/cufflink/${name} \
+ -G ${gtf}/genes.gtf -b${genome}/genome.fa \
+ -u --library-type fr-unstranded \
+${dir}/${name}/accepted_hits.bam \
+
+echo ${name}
