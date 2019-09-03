9c9958679624a13ae510e592fa22e3fdf69f37e1
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2019-09-03 15:42:09 +0200

    Create Feature_count.sh

---
+++ RNA-seq counts/Feature_count.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#PBS -N f_count 
+##PBS -j oe 
+##PBS -l file=100GB
+#PBS-l mem=25G
+#PBS -l walltime=50:13:59
+#PBS -l nodes=1:ppn=8   
+#PBS -o ./f_count_$PBS_JOBID.out
+#PBS -e ./f_count_$PBS_JOBID.err
+#PBS -V 
+
+#echo $PBS_JOBNAME
+#echo $PBS_JOBID
+
+
+
+name=$1
+gtf=~/genome/subread_genome/mouse
+out=~/subread
+data=~/subread/count
+featureCounts -p -s 2 -T 16 -a ${gtf}/Mus_musculus.GRCm38.90.gtf -t exon -g gene_id -o ${data}/${name}_f_counts.txt ${out}/${name}_sort_accepted_hits.bam
+
+###clean the data that would take directly to the DEseq2
+
+cut -f1,7-15 ${data}/${name}_f_counts.txt |sed 1d >${data}/${name}_counts_clean.txt
+#cut -f 1,7-15 ${data}/${name}_f_counts.txt > ${data}/${name}_counts_clean.txt
+#cat -n text.txt | sed '11d' 
+echo ${name}
