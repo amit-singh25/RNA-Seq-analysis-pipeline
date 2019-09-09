d506eddb844b3cba63355918d1468312d3ba8b6c
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2019-09-09 21:54:41 +0200

    Update tophat.sh

--- Alignment/tophat.sh
+++ Alignment/tophat.sh
@@ -1,21 +1,16 @@
+#!/bin/bash
+#PBS -N topha
 #PBS-l mem=100G
 #PBS -l walltime=50:13:59
 #PBS -l nodes=1:ppn=8	
-#PBS -o tophat.stdout
-#PBS -e tophat.stderr
 #PBS -o ./tophat_$PBS_JOBID.out
 #PBS -e ./tophat_$PBS_JOBID.err
-
-
-
 echo $PBS_JOBID
 echo $PBS_JOBNAME
 cd $PBS_O_WORKDIR
 
-
 #echo "=========================================================="
 #echo "Starting on : $(date)"
-
 #echo "Running on node : $(hostname)"
 #echo "Current directory : $(pwd)"
 #echo "Current job ID : $JOB_ID"
@@ -23,9 +18,7 @@ cd $PBS_O_WORKDIR
 #echo "Task index number : $SGE_TASK_ID"
 #echo "=========================================================="
 
-
 name=$1
-
 index=~/genome/igenome/Mus_musculus/Ensembl/GRCm38/Sequence/Bowtie2Index
 gtf=~/genome/igenome/Mus_musculus/Ensembl/GRCm38/Annotation/Archives/archive-current/Genes
 input~/mouse_data
@@ -36,7 +29,6 @@ tophat2 -o ${out}/${name} -p 8 -g 2 \
 --transcriptome-index= ${index}/genome ${input}/${name}_R1.fastq.gz ${input}/${name}_R2.fastq.gz
 
 echo $name
-
 
 
 #stat -j $JOB_ID
