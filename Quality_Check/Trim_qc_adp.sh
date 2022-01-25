2eeddcb19cad83441ad84bb84a91ddcf6938e7e5
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2022-01-25 14:09:29 +0100

    Update Trim_qc_adp.sh

--- Quality_Check/Trim_qc_adp.sh
+++ Quality_Check/Trim_qc_adp.sh
@@ -17,7 +17,6 @@ data=~/raw_data
 
 flexbar -r ${name}_R1.fastq.gz -p ${name}_R2.fastq.gz -t ${data}/${name}_trimmed.fastq  -n 20 -z GZ -m 30 -u 0  -q TAIL -qt 28 -a ${data}/adaptor.fa -qf sanger -j
 
-
 ###find adapator from the fasta_file or else take a illumina adaptor sequence
 ##bbmerge.sh in1=r1.fq in2=r2.fq outa=adapters.fa strict
 ###
