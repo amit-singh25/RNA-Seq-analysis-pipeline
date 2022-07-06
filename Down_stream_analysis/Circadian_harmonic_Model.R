f1ae5b19e0d5318a1fd9d2a747ac49f31e4b5a0b
AMIT SINGH <54843065+amit-singh25@users.noreply.github.com>
2022-07-06 14:56:59 +0200

    Update Circadian_harmonic_Model.R

--- Down_stream_analysis/Circadian_harmonic_Model.R
+++ Down_stream_analysis/Circadian_harmonic_Model.R
@@ -1,6 +1,5 @@
 ####norm data of dl-wt 
 #data<- log2(norm[,c(2:14)]+1)
-
 data<-  log2(norm[,c(2:14)]+1)
 #x<-colData(fullData)$time[c(2:14)]
 x<-colData(fullData)$time[c(2:6,9:14)]
@@ -11,7 +10,6 @@ x<-colData(fullData)$time[c(2:6,9:14)]
   phase <- as.vector( c() )
  for( i in 1:nrow( data ) ){
  y <- as.numeric( data[i,1:11 ] )
-
 fit1 <- lm( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
 fit0 <- lm( y ~ x )
 
@@ -48,12 +46,9 @@ df<-df[,1:2]
 colnames(df)<-c("ens.id","symbol")
 df<-df[!duplicated(df), ]
 sig<-as.data.frame( cbind( sig,df[!duplicated(df), ]))
-
 save(sig,file="final_with_out_11_with_out_0_log.rda")
 
-
-
-####
+#### creat aplot with html output
 sig<-sig[,c(25:30)]
 page <- openPage( "ryth_gene_final.html",
                   head = paste( sep="\n",
@@ -74,20 +69,16 @@ cat( file=page,
 closePage(page)
 browseURL( "ryth_gene_final.html" )
 
-######
+###### useing deseq2
 dd <- fullData[,fullData$samplelabel=="dd-wt"]
 dd$phase <- as.integer( dd$time) / 22 * 2*pi
 #colData( dd)$condition<-as.factor( c( "0h","2h","4h", "6h","8h","10h","12h","14h","16h","18h","20h","22h") )
 colData( dd)$samplelabel<-as.factor(( dd)$samplelabel)
 design = model.matrix( ~ dd$condition*( dd$time + cos( dd$phase) + sin( dd$phase) ))
 #design = model.matrix( ~ dd$samplelabel*( dd$time + cos( dd$phase) + sin( dd$phase) ))
-
-
 #y<- DGEList( counts = assay( dd))
-
 #design <- ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi )
 #table(rowSums(y$counts==0)==9)
-
 keep <- rowSums( cpm( y) > 0.5) >= 2
 table( keep)
 #keep
@@ -95,9 +86,7 @@ y<-y[keep, ,keep.lib.sizes=FALSE]
 y <- calcNormFactors( y)
 v <- voom( y, design, plot=TRUE)
 efit <- eBayes( vfit)
-
 #######polar plot 
-
 df<-melt(dat, id=c("gene","phase"))
 ggplot(data=df,aes(x=df$gene,y=df$pahse,gorp=varia))+
       geom_bar(stat="identity")+
@@ -124,7 +113,6 @@ rownames(newmat)<-rownames(mat)
 colnames(newmat)<-c("0hr","2hr","4hr","6hr","8hr","10hr","12hr","14hr","16hr","18hr","20hr","22hr")
 save(newmat,file="dark_rep_mean.rda")
 #newmat<-log2(newmat[,c(1:12)]+1)
-
 x<-colData(fullData)$time[c(29:40)]
 #data<-newmat[,c(1:12)]
 ##
@@ -132,23 +120,22 @@ approx <- as.data.frame( mat.or.vec( nrow( newmat), 15))
 pval<- as.vector( c() )
 amplitute <- as.vector( c() )
 phase <- as.vector( c() )
- 
- for( i in 1:nrow( newmat ))  {
-  y <- as.numeric( newmat[i,1:12 ] )
-  fit1 <- lm( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
-  fit0 <- lm( y ~ x )
-  #fit1<-glm.nb( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
-  #fit0 <-glm.nb( y ~ x )
-  amplitute[i]<- sqrt( coef(fit1 )[3]^2 + coef( fit1 )[4]^2 )
-  #phase[i]<- atan2( coef(fit1)[4], coef( fit1)[3] )
-  phase[i]=atan2( coef( fit1 )[4], coef( fit1 )[3] )*22/( 2*pi )
-  model<-anova( fit0, fit1 )
-  #model<-anova( fit0, fit1, test="Chisq" )
-  #pval[i]<-model$`Pr(>F)`[2]
+for( i in 1:nrow( newmat ))  {
+y <- as.numeric( newmat[i,1:12 ] )
+fit1 <- lm( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
+fit0 <- lm( y ~ x )
+#fit1<-glm.nb( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
+#fit0 <-glm.nb( y ~ x )
+amplitute[i]<- sqrt( coef(fit1 )[3]^2 + coef( fit1 )[4]^2 )
+#phase[i]<- atan2( coef(fit1)[4], coef( fit1)[3] )
+phase[i]=atan2( coef( fit1 )[4], coef( fit1 )[3] )*22/( 2*pi )
+model<-anova( fit0, fit1 )
+#model<-anova( fit0, fit1, test="Chisq" )
+#pval[i]<-model$`Pr(>F)`[2]
 pval[i]<-model$`Pr(>F)`[2]
-  #pval[i]<-model[["Pr(Chi)"]][2]
+#pval[i]<-model[["Pr(Chi)"]][2]
 print(i)
- }
+}
 approx <- as.data.frame( cbind(newmat, pval, amplitute, phase ) )
 #approx$p.adj<-p.adjust( pval, method = "BH",n = length( pval ) )
 approx$p.adj<-p.adjust( approx$pval, method = "BH",n = length( approx$pval ) )
@@ -175,10 +162,8 @@ save(sig,file="ryth_dd_rep_with_out_0_log.rda")
 
 #######
 index=(apply(newmat,1,sum)>20) 
-
 row_sub = apply(newmat, 1, function(row) all(row !=0 ))
 data<-newmat[index,]
-
 approx <- as.data.frame( mat.or.vec( nrow(data), 15))
 pval<- as.vector( c() )
 amplitute <- as.vector( c() )
@@ -188,15 +173,14 @@ for( i in 1:nrow( data ))  {
   y <- as.numeric( data[i,1:12 ] )
   #fit1 <- lm( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
   #fit0 <- lm( y ~ x )
-  fit1<-glm.nb( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
-  fit0 <-glm.nb( y ~ x )
-  amplitute[i]<- sqrt( coef(fit1 )[3]^2 + coef( fit1 )[4]^2 )
-  #phase[i]<- atan2( coef(fit1)[4], coef( fit1)[3] )
-  phase[i]=atan2( coef( fit1 )[4], coef( fit1 )[3] )*22/( 2*pi )
-  model<-anova( fit0, fit1 )
-  model<-anova( fit0, fit1, test="Chisq" )
-  pval[i]<-model$`Pr(>F)`[2]
-  #pval[i]<-model$`Pr(>F)`[2]
+fit1<-glm.nb( y ~ x + cos( x/22*2*pi ) + sin( x/22*2*pi ) )
+fit0 <-glm.nb( y ~ x )
+amplitute[i]<- sqrt( coef(fit1 )[3]^2 + coef( fit1 )[4]^2 )
+#phase[i]<- atan2( coef(fit1)[4], coef( fit1)[3] )
+phase[i]=atan2( coef( fit1 )[4], coef( fit1 )[3] )*22/( 2*pi )
+model<-anova( fit0, fit1 )
+model<-anova( fit0, fit1, test="Chisq" )
+pval[i]<-model$`Pr(>F)`[2]  #pval[i]<-model$`Pr(>F)`[2]
   #pval[i]<-model[["Pr(Chi)"]][2]
 #  approx <- as.data.frame( cbind( data, pval, amplitute, phase ) )
  # approx$p.adj<-p.adjust( approx$pval, method = "BH",n = length( approx$pval ) )
